5ac46757a76f64b35d4862a8a73d3e8cd0a98529
Amrnaassar <nassaramr73@gmail.com>
2025-01-14 10:24:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,8 @@ setInterval(() => {
     });
 }, 5 * 60 * 1000); // كل 5 دقائق
 
+const PORT = process.env.PORT || 3000;
 // تشغيل السيرفر
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port '+PORT);
 });
