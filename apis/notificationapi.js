28b199237be5547507b5be06e840bbf058d3b6e8
Amrnaassar <nassaramr73@gmail.com>
2025-02-16 14:43:41 +0400

    Update notificationapi.js

--- apis/notificationapi.js
+++ apis/notificationapi.js
@@ -57,4 +57,23 @@ module.exports = (app) => {
       });
       
 
+      app.delete('/api/deleteNotification/:id', async (req, res) => {
+        try {
+          const { id } = req.params;
+          
+          // البحث عن الإشعار وحذفه
+          const deletedNotification = await Notification.findByIdAndDelete(id);
+      
+          if (!deletedNotification) {
+            return res.status(404).json({ message: 'Notification not found' });
+          }
+      
+          res.status(200).json({ message: 'Notification deleted successfully' });
+        } catch (error) {
+          console.error('Error deleting notification:', error);
+          res.status(500).json({ message: 'Failed to delete notification' });
+        }
+      });
+      
+
 };
